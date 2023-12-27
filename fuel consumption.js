c23d28fdf344a1feaf062be62b076025b9d84ddb
Altieri Palorca <altierirpalorca@gmail.com>
2023-12-26 22:51:11 -0300

    1.1 - css simplificado - js começado primeiros cálculos

--- fuel consumption.js
+++ fuel consumption.js
@@ -1,3 +1,14 @@
-let valorKm = document.querySelectorAll("#valorComb")
+let valorKm = document.querySelectorAll("#valorComb")[0];
 
-let dist = document.querySelectorAll("#kmPercorrido")
+let dist = document.querySelectorAll("#kmPercorrido")[0];
+
+let result = document.querySelectorAll("#resultado")[0];
+
+result.addEventListener("click" , function() {
+    let valorFinal = (valorKm.value) * (dist.value)
+    console.log("O valor do consumo é", valorFinal)
+    
+});
+
+
+//inserir cálculo baseado no consumo do carro
