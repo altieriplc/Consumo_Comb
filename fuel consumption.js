08f23720428f847b44ebb376f818df60e862c245
Altieri Palorca <altierirpalorca@gmail.com>
2024-01-05 17:03:00 -0300

    1.3 - css com teste bagunçado figma

--- fuel consumption.js
+++ fuel consumption.js
@@ -2,16 +2,20 @@ let valorKm = document.querySelectorAll("#valorComb")[0];
 
 let dist = document.querySelectorAll("#kmPercorrido")[0];
 
-let result = document.querySelectorAll("#resultado")[0];
+let result = document.querySelectorAll("#botaoresultado")[0];
 
 let consumoCarro = document.querySelector("#consCarro");
 
 result.addEventListener("click" , function() {
     let valorFinal = (dist.value / consumoCarro.value) * (valorKm.value)
-    console.log("O valor do consumo é", valorFinal.toFixed(2))
-    
+    console.log("O valor do consumo é", valorFinal.toFixed(2)) 
 });
 
+let testeFigma = document.querySelector('#figma')
+
+testeFigma.addEventListener("click", function(){
+    console.log("TE AMO")
+});
 
 //criar objetos para modelos dos carros dentro
 //criar acesso para valor de consumo dentro desse objeto
