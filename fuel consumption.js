18bc04b489e8046d2fde12940735971380517be3
Altieri Palorca <altierirpalorca@gmail.com>
2024-01-22 18:08:07 -0300

    1.6 - css melhorado - js incompleto

--- fuel consumption.js
+++ fuel consumption.js
@@ -1,3 +1,16 @@
+// let testeFigma = document.querySelector('#figma')
+
+// testeFigma.addEventListener("click", function(){
+//     console.log("TE AMO")
+// });
+
+//criar objetos para modelos dos carros dentro
+//criar acesso para valor de consumo dentro desse objeto
+
+
+
+// window.alert("Resultado no Console")
+
 let valorKm = document.querySelectorAll("#valorComb")[0];
 
 let dist = document.querySelectorAll("#kmPercorrido")[0];
@@ -8,14 +21,46 @@ let consumoCarro = document.querySelector("#consCarro");
 
 result.addEventListener("click" , function() {
     let valorFinal = (dist.value / consumoCarro.value) * (valorKm.value)
-    console.log("O valor do consumo é R$",valorFinal.toFixed(2)) 
+    console.log("O valor do combustível para essa viagem é de R$",valorFinal.toFixed(2)) 
 });
 
-let testeFigma = document.querySelector('#figma')
+let opcoesDeCarros = [
+    {
+        nome: "Clio",
+        marca: "Renault",
+        consumo: 14.3,
 
-testeFigma.addEventListener("click", function(){
-    console.log("TE AMO")
-});
+    },
 
-//criar objetos para modelos dos carros dentro
-//criar acesso para valor de consumo dentro desse objeto
+    {
+        nome:"Kwid",
+        marca: "Renault",
+        consumo: 15.7,
+    }
+    
+
+]
+
+let selecCarro = document.getElementById("selecionarCarro") //pegando o select
+
+selecCarro.addEventListener("change",function(){ //função pega o valor escolhido no select
+
+    let carroSelecionado = selecCarro.consumo.value;
+
+    for(let i = 0; i < opcoesDeCarros.length; i++){ //percorre o array
+
+        if(i == carroSelecionado){ //se o i for igual ao que escolhi no select
+            
+            
+        }
+    
+    
+   
+    
+    // if (carroSelecionado) {
+    //     let resultado = (dist.value / carroSelecionado.consumo) * valorKm.value;
+    //     console.log("O valor do combustível para essa viagem é de R$", resultado.toFixed(2));
+    // } else {
+    //     console.log("Carro não encontrado");
+    // }
+}})
