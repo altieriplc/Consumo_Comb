c0ac675c505feed41de84214a0ef41470b5ae6cb
Altieri Palorca <altierirpalorca@gmail.com>
2024-02-26 16:53:01 -0300

    2.5 - js corrigido, melhorias de código

--- arrays.mjs
+++ arrays.mjs
@@ -1,31 +1,31 @@
 export let opcoesDeCarros = [
     {
-        none:0
+        none: 0
     },
     {
         nome: "Clio",
         marca: "Renault",
         consumo: 14.3,
-        
+
     },
-    
+
     {
-        nome:"Kwid",
+        nome: "Kwid",
         marca: "Renault",
         consumo: 15.7,
     },
     {
-        nome:"T6",
+        nome: "T6",
         marca: "Jac",
         consumo: 10.0,
     },
     {
-        nome:"Onix",
+        nome: "Onix",
         marca: "GM",
         consumo: 13.3,
     },
     {
-        nome:"Pajero",
+        nome: "Pajero",
         marca: "Mitisubishi",
         consumo: 6.0,
     }
--- fuel consumption.mjs
+++ fuel consumption.mjs
@@ -1,53 +1,45 @@
 
 
 
+import { opcoesDeCarros } from "./arrays.mjs";
+
 //window.alert("Resultado no Console")
 
 
-import { opcoesDeCarros } from "./arrays.mjs";
-
 //window.alert('Os valores podem não ser exatos, tanto pelo consumo informado, quanto pela situação mecânica de cada veículo')
 
-let dist = document.querySelector("#kmPercorrido")[0]; //CAPTURA O KM PERCORRIDO
+let dist = document.getElementById("kmPercorrido"); //CAPTURA O KM PERCORRIDO
 
-let valorKm = document.querySelector("#valorComb")[0]; //CAPTURA O INPUT DO VALOR DO COMBUSTÍVEL
+let valorKm = document.getElementById("valorComb"); //CAPTURA O INPUT DO VALOR DO COMBUSTÍVEL
 
-let consumoCarro = document.querySelector("#consCarro"); //CAPTURA O CONSUMO DO COMBUSTÍVEL
+let consumoCarro = document.getElementById("consCarro"); //CAPTURA O CONSUMO DO COMBUSTÍVEL
 
-let result = document.querySelectorAll("#botaoresultado")[0]; //CAPTURA O BOTÃO CALCÚLAR
+let result = document.getElementById("botaoresultado"); //CAPTURA O BOTÃO CALCÚLAR
 
 let selecCarro = document.getElementById("selecionarCarro"); //CAPTURA O SELECT
 
-let idaEvolta = document.querySelector('#idaVolta') //CAPTURA O CHECKED DO BOTÃO IDA E VOLTA
+let idaEvolta = document.getElementById('idaVoltaButton') //CAPTURA O CHECKED DO BOTÃO IDA E VOLTA
 
 //--------------------------------------------------
 
-idaEvolta.addEventListener("change", function(){//CAPTURA BOTÃO IDA E VOLTA
+idaEvolta.addEventListener("change", function () {//CAPTURA BOTÃO IDA E VOLTA
 });
 
 //--------------------------------------------------
 
-result.addEventListener("click" , function(event) { // FUNÇÃO DO EVENTO CLICK NO BOTÃO CALCULAR
+result.addEventListener("click", function (event) { // FUNÇÃO DO EVENTO CLICK NO BOTÃO CALCULAR
     event.preventDefault(); // Evita o comportamento padrão do formulário de recarregar a página
 
     let valorFinal = (dist.value / consumoCarro.value) * (valorKm.value)
-    if (consumoCarro.value && valorKm.value && dist.value && idaEvolta.checked) {
+    if (idaEvolta.checked && consumoCarro.value && valorKm.value && dist.value) {
         // SE OS 4 CAMPOS ESTIVEREM PREENCHIDOS
-        return console.log("O valor do combustível para essa viagem é de R$",(valorFinal * 2).toFixed(2))
+        return console.log("O valor do combustível para essa viagem é de R$", (valorFinal * 2).toFixed(2))
 
-    }else if (consumoCarro.value && valorKm.value && dist.value) {
+    } else if (consumoCarro.value && valorKm.value && dist.value) {
         // SE OS 3 CAMPOS ESTIVEREM PREENCHIDOS
-        return console.log("O valor do combustível para essa viagem é de R$",(valorFinal).toFixed(2))
+        return console.log("O valor do combustível para essa viagem é de R$", (valorFinal).toFixed(2))
     }
-    else if(selecCarro.selectedIndex !== 0){
-        // Se o campo de consumo do carro não estiver preenchido, mas um carro foi selecionado no select
-        let indiceSelecionado = selecCarro.selectedIndex;
-        let carroSelecionado = opcoesDeCarros[indiceSelecionado];
-        
-        console.log("Carro selecionado:", carroSelecionado.nome);
-        console.log("O valor do combustível para essa viagem é de: R$",((dist.value / carroSelecionado.consumo) * valorKm.value).toFixed(2));
-        
-    }else{
+     else {
         // Se nenhum carro foi selecionado e o campo de consumo do carro está vazio
         window.alert("Por favor, selecione um carro no menu e preencha Km e preço do combustível, ou preencha os três campos para o cálculo ser feito.");
     }
@@ -59,30 +51,37 @@ result.addEventListener("click" , function(event) { // FUNÇÃO DO EVENTO CLICK 
 
 
 
-selecCarro.addEventListener("change",function(){ //função pega o valor escolhido no select
-    
-    if(dist.value.length === 0){// VERIFICA SE O CAMPO ESTÁ PREENCHIDO
+selecCarro.addEventListener("change", function () { //função pega o valor escolhido no select
+
+    if (dist.value.length === 0) {// VERIFICA SE O CAMPO DISTÂNCIA ESTÁ PREENCHIDO
         return window.alert("Preencha a quantidade de Km's e o preço do combustível para o cálculo")
-        
-    }else if(valorKm.value.length === 0){// VERIFICA SE O CAMPO ESTÁ  PREENCHIDO
+
+    } else if (valorKm.value.length === 0) {// VERIFICA SE O CAMPO ESTÁ  PREENCHIDO
         return window.alert("Preencha a quantidade de Km's e o preço do combustível para o cálculo")
         //SE FALTAR CAMPO PREENCHIDO RETORNA AS MENSAGENS, CASO NÃO FALTA SEGUE PARA O ELSE E TERMINA DE EXECUTAR O CÁLCULO
-        
-    }else{
-        
+
+    } else {
+
         let indiceSelecionado = selecCarro.selectedIndex; // Obtendo o índice da opção selecionada
-        
+
         let carroSelecionado = opcoesDeCarros[indiceSelecionado]; // Obtendo o objeto do array com base no índice
         // Agora você pode acessar as propriedades do carro selecionado, por exemplo:
-        
-        if(idaEvolta.checked){
-            console.log("Carro selecionado:", carroSelecionado.nome);
-            return console.log("O valor do combustível para essa viagem é de: R$", (((dist.value / carroSelecionado.consumo) * (valorKm.value)).toFixed(2)) * 2)
+        if (selecCarro.selectedIndex == [0]) {
+            return console.log('Escola um veículo')
+
         }
-        
+
+
+        if (idaEvolta.checked) {
+            console.log("Carro selecionado:", carroSelecionado.nome);
+            return console.log(`O valor do combustível para ${carroSelecionado.nome} nessa viagem é de: R$`, (((dist.value / carroSelecionado.consumo) * (valorKm.value)).toFixed(2)) * 2)
+        }
+
         console.log("Carro selecionado:", carroSelecionado.nome);
-        console.log("O valor do combustível para essa viagem é de: R$", ((dist.value / carroSelecionado.consumo) * (valorKm.value)).toFixed(2));  
+        console.log(`O valor do combustível para ${carroSelecionado.nome} nessa viagem é de: R$`, ((dist.value / carroSelecionado.consumo) * (valorKm.value)).toFixed(2));
     }
 })
 
 
+//OPÇÃO DE INSERIR VALOR DO PEDÁGIO
+//OPÇÃO DE DIVIDR POR QUANTAS PESSOAS VÃO VIAJAR
